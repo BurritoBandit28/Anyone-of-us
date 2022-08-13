5204a7ef2bfda1e4a73b7e51343ee77df278c283
BurritoBandit28 <yansalmon@gmail.com>
2022-08-13 14:39:00 +0100

    some extra bits
    aaaaaaaaaaaaaaaaaaaaaaaaaaggghh

--- src/main/java/io/github/burritobandit28/any1_of_us/items/ItemRegister.java
+++ src/main/java/io/github/burritobandit28/any1_of_us/items/ItemRegister.java
@@ -2,6 +2,7 @@ package io.github.burritobandit28.any1_of_us.items;
 
 import io.github.burritobandit28.any1_of_us.AnyoneOfUs;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -11,13 +12,6 @@ public class ItemRegister {
 
 		Registry.register(Registry.ITEM, AnyoneOfUs.ID("knife"), ModItems.KNIFE);
 
-		FabricModelPredicateProviderRegistry.register(ModItems.KNIFE, new Identifier("backstab"), ((itemStack, clientWorld, livingEntity, i) -> {
-			int val = 0;
-			if (itemStack.getNbt() != null && itemStack.getNbt().contains("Backstab")) {
-				val = itemStack.getNbt().getInt("Backstab");
-			}
-			return val;
-		}));
 
 		Registry.register(Registry.ITEM, AnyoneOfUs.ID("cloaking_device"), ModItems.CLOAKING_DEVICE);
 
---
+++ src/main/java/io/github/burritobandit28/any1_of_us/mixins/PlayerEntityRendererMixin.java
@@ -0,0 +1,38 @@
+package io.github.burritobandit28.any1_of_us.mixins;
+
+import io.github.burritobandit28.any1_of_us.items.ModItems;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Objects;
+
+@Mixin(PlayerEntityRenderer.class)
+public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>> {
+
+
+	public PlayerEntityRendererMixin(EntityRendererFactory.Context context, PlayerEntityModel<AbstractClientPlayerEntity> entityModel, float f) {
+		super(context, entityModel, f);
+	}
+
+	@Inject(at = @At("TAIL"), method = "getArmPose", cancellable = true)
+	private static void doArmMovement(AbstractClientPlayerEntity player, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir) {
+
+		ItemStack itemStack = player.getStackInHand(hand);
+
+
+		if (itemStack.getItem() == ModItems.KNIFE && Objects.requireNonNull(itemStack.getNbt()).getInt("Backstab") == 1) {
+			cir.setReturnValue(BipedEntityModel.ArmPose.SPYGLASS);
+		}
+	}
+
+}
