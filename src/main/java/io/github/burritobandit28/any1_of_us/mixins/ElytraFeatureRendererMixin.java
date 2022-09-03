624e1ea52a4962c8724425a6b590f88ce4abfcdc
BurritoBandit28 <yansalmon@gmail.com>
2022-09-03 10:08:22 +0100

    Added support for elytra when cloaking

--- src/main/java/io/github/burritobandit28/any1_of_us/mixins/ElytraFeatureRendererMixin.java
+++ src/main/java/io/github/burritobandit28/any1_of_us/mixins/ElytraFeatureRendererMixin.java
@@ -1,16 +1,16 @@
 package io.github.burritobandit28.any1_of_us.mixins;
 
-import io.github.burritobandit28.any1_of_us.AnyoneOfUs;
 import io.github.burritobandit28.any1_of_us.effects.CloakedStatusEffect;
-import io.github.burritobandit28.any1_of_us.items.CloakingDeviceItem;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
+import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
-import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
-import org.quiltmc.qsl.networking.api.client.ClientPlayNetworking;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,17 +20,14 @@ import java.util.Objects;
 
 import static io.github.burritobandit28.any1_of_us.AnyoneOfUsClient.isCloaked;
 
-@Mixin(ArmorFeatureRenderer.class)
-public abstract class ArmorFeatureRendererMixin <T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> extends FeatureRenderer<T, M> {
-
-	public ArmorFeatureRendererMixin(FeatureRendererContext<T, M> featureRendererContext) {
+@Mixin(ElytraFeatureRenderer.class)
+public abstract class ElytraFeatureRendererMixin<T extends LivingEntity, M extends EntityModel<T>> extends FeatureRenderer<T, M> {
+	public ElytraFeatureRendererMixin(FeatureRendererContext<T, M> featureRendererContext) {
 		super(featureRendererContext);
 	}
 
 	@Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/LivingEntity;FFFFFF)V", cancellable = true)
-	public void cancelArmorRender(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
-
-
+	public void cancelElytraRender(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, T livingEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci) {
 
 		for (int iii = 0; iii < isCloaked.size() && isCloaked.get(iii) != null; iii++) {
 			if (Objects.equals(isCloaked.get(iii).name, livingEntity.getEntityName())) {
@@ -44,6 +41,4 @@ public abstract class ArmorFeatureRendererMixin <T extends LivingEntity, M exten
 		}
 
 	}
-
-
 }
